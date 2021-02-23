14f09ebccc82934d3a0b92011d0632e07467222a
ByronStar <benno@fam-staebler.de>
2021-02-23 14:27:59 +0100

    Starter Paket fast komplett

--- STEP/js/ps_step_1.js
+++ STEP/js/ps_step_1.js
@@ -1,12 +1,4 @@
 Homeworks.aufgabe = 1;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight)
-}
-
-function draw() {
-  background(255, 10);
-  let c = color(Math.random() * 256, Math.random() * 256, Math.random() * 256);
-  fill(c);
-  ellipse(Math.random() * windowWidth, Math.random() * windowHeight, 20, 20);
+function init() {
 }
---
+++ STEP/js/ps_step_3.js
@@ -0,0 +1,9 @@
+Homeworks.aufgabe = 3;
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight)
+  canvas.parent('thecanvas')
+}
+
+function draw() {
+}
