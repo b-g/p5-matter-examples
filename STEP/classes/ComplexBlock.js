ba3cd4312d659cd65902fbd006bf83e73c6ee23f
ByronStar <benno@fam-staebler.de>
2021-09-24 16:57:57 +0200

    Reworked STEP based on new classes API

--- STEP/classes/ComplexBlock.js
+++ STEP/classes/ComplexBlock.js
@@ -1,12 +1,11 @@
 /*
 Usage:
 // define block parts via parts array option
-let block = new ComplexBlock({ x: 900, y: 730, color: 'blue' }, { parts: [ Bodies.rectangle(4, 20, 5, 20), Bodies.rectangle(40 - 4, 20, 5, 20), Bodies.rectangle(20, +40 - 4, 50, 5) ], isStatic: true });
+let block = new ComplexBlock(world, { x: 900, y: 730, color: 'blue' }, { parts: [ Bodies.rectangle(4, 20, 5, 20), Bodies.rectangle(40 - 4, 20, 5, 20), Bodies.rectangle(20, +40 - 4, 50, 5) ], isStatic: true });
 */
 class ComplexBlock extends Block {
-
-  constructor(attrs, options) {
-    super(attrs, options);
+  constructor(world, attrs, options) {
+    super(world, attrs, options);
   }
 
   addBody() {
--- STEP/js/ps_step_7.js
+++ STEP/js/ps_step_7.js
@@ -4,11 +4,11 @@ const Engine = Matter.Engine;
 const Bodies = Matter.Bodies;
 const Events = Matter.Events;
 const World = Matter.World;
-const Mouse = Matter.Mouse;
-const MouseConstraint = Matter.MouseConstraint;
 
 // the Matter engine to animate the world
 let engine;
+let world;
+let mouse;
 // an array to contain all the blocks created
 let blocks = [];
 
@@ -17,81 +17,100 @@ function setup() {
   canvas.parent('thecanvas');
 
   engine = Engine.create();
+  world = engine.world;
 
   // create some blocks
   // the blue box triggers a function on collisions
-  blocks.push(new BaseBlock({
-    x: 200, y: 200, w: 60, h: 60,
-    color: 'blue',
-    trigger: (ball, block) => {
-      // console.log("Trigger ", ball, block, puzzle);
-      puzzle.attrs.color = 'magenta';
-    }
-  },
+  blocks.push(new BaseBlock(
+    world,
+    {
+      x: 200, y: 200, w: 60, h: 60,
+      color: 'blue',
+      trigger: (ball, block) => {
+        // console.log("Trigger ", ball, block, puzzle);
+        puzzle.attrs.color = 'magenta';
+      }
+    },
     { isStatic: false, density: 0.5 }
   ));
 
-  const fixed1 = new Block({
-    x: 700, y: 200, w: 40, h: 40,
-    color: 'cyan'
-  },
+  const fixed1 = new Block(
+    world,
+    {
+      x: 700, y: 200, w: 40, h: 40,
+      color: 'cyan'
+    },
     { isStatic: false }
   );
   fixed1.constrainTo(null, { pointB: { x: 900, y: 300 }, length: 300 });
   blocks.push(fixed1);
 
-  const fixed2 = new Block({
-    x: 800, y: 200, w: 40, h: 40,
-    color: 'cyan'
-  },
+  const fixed2 = new Block(
+    world,
+    {
+      x: 800, y: 200, w: 40, h: 40,
+      color: 'cyan'
+    },
     { isStatic: false }
   );
   fixed2.constrainTo(fixed1, { length: 150, stiffness: 0.1 });
   blocks.push(fixed2);
 
-  blocks.push(new BaseBlock({
-    x: windowWidth / 2, y: 800, w: windowWidth, h: 10,
-    color: 'gray'
-  },
+  blocks.push(new BaseBlock(
+    world,
+    {
+      x: windowWidth / 2, y: 800, w: windowWidth, h: 10,
+      color: 'gray'
+    },
     { isStatic: true }
   ));
 
-  blocks.push(new BaseBlock({
-    x: -100, y: 450, w: 800, h: 10,
-    color: 'gray'
-  },
+  blocks.push(new BaseBlock(
+    world,
+    {
+      x: -100, y: 450, w: 800, h: 10,
+      color: 'gray'
+    },
     { angle: PI / 3, isStatic: true }
   ));
 
-  blocks.push(new BaseBlock({
-    x: windowWidth + 100, y: 450, w: 800, h: 10,
-    color: 'gray'
-  },
+  blocks.push(new BaseBlock(
+    world,
+    {
+      x: windowWidth + 100, y: 450, w: 800, h: 10,
+      color: 'gray'
+    },
     { angle: -PI / 3, isStatic: true }
   ));
 
   // the ball has a label and can react on collisions
-  blocks.push(new Ball({
-    x: 300, y: 300, r: 30,
-    color: 'magenta'
-  },
+  blocks.push(new Ball(
+    world,
+    {
+      x: 300, y: 300, r: 30,
+      color: 'magenta'
+    },
     { label: "Murmel" }
   ));
 
   // create a rotating block - propeller
-  blocks.push(new Block({
-    x: 640, y: 440, w: 100, h: 5,
-    color: 'white',
-    rotate: { angle: 0, delta: 0.07 }
-  },
+  blocks.push(new Block(
+    world,
+    {
+      x: 640, y: 440, w: 100, h: 5,
+      color: 'white',
+      rotate: { angle: 0, delta: 0.07 }
+    },
     { isStatic: true }
   ));
 
   // create a body from multiple parts
-  blocks.push(new ComplexBlock({
-    x: 900, y: 730,
-    color: 'blue'
-  },
+  blocks.push(new ComplexBlock(
+    world,
+    {
+      x: 900, y: 730,
+      color: 'blue'
+    },
     {
       parts: [
         Bodies.rectangle(4, 20, 5, 20),
@@ -104,58 +123,61 @@ function setup() {
   ));
 
   // create a body from points
-  blocks.push(new FreehandBlock({
-    x: 600,
-    y: 580,
-    points: [
-      { x: 0, y: 0 }, { x: 20, y: 10 }, { x: 200, y: 30 }, { x: 220, y: 50 }, { x: 10, y: 20 }
-    ],
-    color: 'olive'
-  }, { isStatic: true }));
+  blocks.push(new FreehandBlock(
+    world,
+    {
+      x: 600,
+      y: 580,
+      points: [
+        { x: 0, y: 0 }, { x: 20, y: 10 }, { x: 200, y: 30 }, { x: 220, y: 50 }, { x: 10, y: 20 }
+      ],
+      color: 'olive'
+    }, { isStatic: true }));
 
   // create a body from a SVG path
   // the puzzle can apply a force on collisions 
-  const puzzle = new SVGBlock({
-    x: 300, y: 500,
-    elem: 'puzzle',
-    scale: 0.6, color: 'lime',
-    force: { x: 0.0, y: -0.04 }
-  },
+  const puzzle = new SVGBlock(
+    world,
+    {
+      x: 300, y: 500,
+      elem: 'puzzle',
+      scale: 0.6, color: 'lime',
+      force: { x: 0.0, y: -0.04 }
+    },
     { isStatic: true, friction: 0.0 }
   );
   blocks.push(puzzle);
 
-  blocks.push(new SVGBlock({
-    x: 580, y: 710,
-    file: './path.svg',
-    scale: 0.6, color: 'yellow'
-  },
+  blocks.push(new SVGBlock(
+    world,
+    {
+      x: 580, y: 710,
+      file: './path.svg',
+      scale: 0.6, color: 'yellow'
+    },
     { isStatic: true, friction: 0.0 }
   ));
 
   // create a group of identical bodies
-  blocks.push(new CompositeBlock({
-    x: 550,
-    y: 100,
-    cols: 10,
-    rows: 10,
-    colGap: 5,
-    rowGap: 5,
-    color: 'red',
-    create: (bx, by) => Bodies.circle(bx, by, 10, { restitution: 0.9 })
-  }, {}));
+  blocks.push(new CompositeBlock(
+    world,
+    {
+      x: 550,
+      y: 100,
+      cols: 10,
+      rows: 10,
+      colGap: 5,
+      rowGap: 5,
+      color: 'red',
+      create: (bx, by) => Bodies.circle(bx, by, 10, { restitution: 0.9 })
+    }, {}));
 
   // add a mouse so that we can manipulate Matter objects
-  mouseConstraint = MouseConstraint.create(engine, {
-    mouse: Mouse.create(canvas.elt),
-    constraint: { stiffness: 0.05, angularStiffness: 0 }
-  });
-  mouseConstraint.mouse.pixelRatio = pixelDensity();
-  World.add(engine.world, mouseConstraint);
+  mouse = new Mouse(engine, canvas, { stroke: 'blue', strokeWeight: 3 });
 
   // process mouseup events in order to - add more balls
-  Events.on(mouseConstraint, "mouseup", evt => {
-    let ball = new Ball({ x: evt.mouse.position.x, y: evt.mouse.position.y, r: 15, color: 'yellow' }, { isStatic: false, restitution: 0.9, label: 'Murmel' });
+  mouse.on("mouseup", evt => {
+    let ball = new Ball(world, { x: evt.mouse.position.x, y: evt.mouse.position.y, r: 15, color: 'yellow' }, { isStatic: false, restitution: 0.9, label: 'Murmel' });
     blocks.push(ball);
   });
 
@@ -185,19 +207,7 @@ function draw() {
 
 function drawConstraint(constraint) {
   if (constraint.label == "Mouse Constraint") {
-    if (mouseConstraint.body) {
-      let pos = mouseConstraint.body.position;
-      let offset = mouseConstraint.constraint.pointB;
-      let m = mouseConstraint.mouse.position;
-      stroke(0, 255, 0);
-      strokeWeight(2);
-      line(
-        pos.x + offset.x,
-        pos.y + offset.y,
-        m.x,
-        m.y
-      );
-    }
+    mouse.draw();
   } else {
     let posA = { x: 0, y: 0 };
     if (constraint.bodyA) {
