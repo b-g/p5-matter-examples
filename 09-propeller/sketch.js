2d517c97084e0a0f0ff561cd616703e0bc973aae
Benedikt Groß <bg@benedikt-gross.de>
2021-09-30 14:58:02 +0200

    Ported to OOP: 09-propeller

--- 09-propeller/sketch.js
+++ 09-propeller/sketch.js
@@ -3,18 +3,6 @@
 // setup wrap coordinates plugin
 Matter.use('matter-wrap');
 
-const Engine = Matter.Engine;
-const Render = Matter.Render;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Mouse = Matter.Mouse;
-const MouseConstraint = Matter.MouseConstraint;
-const Body = Matter.Body;
-
-const drawMouse = Helpers.drawMouse;
-const drawBody = Helpers.drawBody;
-
-let engine;
 let ball1;
 let ball2;
 let propeller;
@@ -25,48 +13,46 @@ function setup() {
   const canvas = createCanvas(800, 600);
 
   // create an engine
-  engine = Engine.create();
+  const engine = Matter.Engine.create();
+  const world = engine.world;
 
-  // add bodies
-  ball1 = Bodies.circle(400, 50, 25, {density: 0.01});
-  ball2 = Bodies.circle(200, 50, 150, {density: 0.0001});
+  // two balls
   const wrap = {
-      min: {x: 0, y: 0},
-      max: {x: width, y: height}
+    min: { x: 0, y: 0 },
+    max: { x: width, y: height }
   };
-  ball1.plugin.wrap = wrap;
-  ball2.plugin.wrap = wrap;
-  propeller = Bodies.rectangle(400, 300, 550, 25, {
-    isStatic: true, angle: angle
-  });
-  World.add(engine.world, [ball1, ball2, propeller]);
+  ball1 = new Ball(world,
+    { x: 400, y: 50, r: 25, color: 'white' },
+    { density: 0.01, plugin: { wrap: wrap } }
+  );
+  ball2 = new Ball(world,
+    { x: 200, y: 50, r: 150, color: 'white' },
+    { density: 0.0001, plugin: { wrap: wrap } }
+  );
+
+  // propeller
+  propeller = new Block(world,
+    { x: 400, y: 300, w: 550, h: 30, color: 'white' },
+    { isStatic: true, angle: angle }
+  );
 
   // setup mouse
-  const mouse = Mouse.create(canvas.elt);
-  const mouseParams = {
-    mouse: mouse,
-    constraint: {stiffness: 0.05}
-  }
-  mouseConstraint = MouseConstraint.create(engine, mouseParams);
-  mouseConstraint.mouse.pixelRatio = pixelDensity();
-  World.add(engine.world, mouseConstraint);
+  mouse = new Mouse(engine, canvas);
 
   // run the engine
-  Engine.run(engine);
+  Matter.Engine.run(engine);
 }
 
 function draw() {
   background(0);
 
-  // angle of propeller
-  Body.setAngle(propeller, angle);
-  Body.setAngularVelocity(propeller, 0.15);
+  // animate angle property of propeller
+  Matter.Body.setAngle(propeller.body, angle);
+  Matter.Body.setAngularVelocity(propeller.body, 0.15);
   angle += 0.07;
 
-  noStroke();
-  fill(255);
-  drawBody(ball1);
-  drawBody(ball2);
-  drawBody(propeller);
-  drawMouse(mouseConstraint);
+  propeller.draw();
+  ball1.draw();
+  ball2.draw();
+  mouse.draw();
 }
