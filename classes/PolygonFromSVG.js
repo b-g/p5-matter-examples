834942ef12b7eb90e7261e6d8b3264b7d73e06d3
ByronStar <benno@fam-staebler.de>
2022-01-24 09:42:30 +0100

    Added better positioning of SVG according to graphical center and added direct path element input

--- classes/PolygonFromSVG.js
+++ classes/PolygonFromSVG.js
@@ -11,24 +11,53 @@ class PolygonFromSVG extends Block {
   }
 
   addBody() {
-    if (this.attrs.fromId) {
-      // find SVG embedded in current HTML document
-      const $element = document.getElementById(this.attrs.fromId);
-      if ($element) {
-        this.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(Matter.Svg.pathToVertices($element, 10), this.attrs.scale, this.attrs.scale), this.options);
-        Matter.Body.setPosition(this.body, this.attrs);
-      }
+    if (this.attrs.fromPath) {
+        // use a path provided directly
+        let vertices = Matter.Svg.pathToVertices(this.attrs.fromPath, 10);
+        this.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(vertices, this.attrs.scale, this.attrs.scale), this.options);
+        Matter.Body.setPosition(this.body, this.getCenter(vertices));
     } else {
-      // load external SVG file
-      let that = this;
-      httpGet(this.attrs.fromFile, "text", false, function(response) {
-        const parser = new DOMParser();
-        const svgDoc = parser.parseFromString(response, "image/svg+xml");
-        const path = svgDoc.querySelector("path");
-        that.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(Matter.Svg.pathToVertices(path, 10), that.attrs.scale, that.attrs.scale), that.options);
-        Matter.Body.setPosition(that.body, that.attrs);
-        Matter.World.add(that.world, that.body);
-      });
+      if (this.attrs.fromId) {
+        // use a path of SVG embedded in current HTML page
+        let path = document.getElementById(this.attrs.fromId);
+        if (null != path) {
+          let vertices = Matter.Svg.pathToVertices(path, 10);
+          this.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(vertices, this.attrs.scale, this.attrs.scale), this.options);
+          if (this.attrs.x) {
+            Matter.Body.setPosition(this.body, this.attrs);
+          } else {
+            Matter.Body.setPosition(this.body, this.getCenter(vertices));
+          }
+        }
+      } else {
+        // use a path in separate SVG file
+        let that = this;
+        httpGet(this.attrs.fromFile, "text", false, function(response) {
+          const parser = new DOMParser();
+          const svgDoc = parser.parseFromString(response, "image/svg+xml");
+          const path = svgDoc.querySelector("path");
+          let vertices = Matter.Svg.pathToVertices(path, 10);
+          that.body = Matter.Bodies.fromVertices(0, 0, Matter.Vertices.scale(vertices, that.attrs.scale, that.attrs.scale), that.options);
+          if (this.attrs.x) {
+            Matter.Body.setPosition(this.body, this.attrs);
+          } else {
+            Matter.Body.setPosition(this.body, this.getCenter(vertices));
+          }
+          Matter.World.add(engine.world, [that.body]);
+        });
+      }
     }
+  }
+
+  getCenter(vertices) {
+    let min = {x: 999999, y: 999999};
+    let max = {x: -999999, y: -999999};
+    vertices.forEach((v, i) => {
+      min.x = min.x > v.x ? v.x : min.x;
+      min.y = min.y > v.y ? v.y : min.y;
+      max.x = max.x < v.x ? v.x : max.x;
+      max.y = max.y < v.y ? v.y : max.y;
+    });
+    return { x: min.x + (this.body.position.x - this.body.bounds.min.x), y: min.y + (this.body.position.y - this.body.bounds.min.y) }
   }
 }
